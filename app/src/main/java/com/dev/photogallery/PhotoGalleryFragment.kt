b14e8091b4c1049f0e18080aca9477e9c46f069e
alice <aliceae.h@gmail.com>
2023-02-10 13:17:08 +0800

    [ch22]create a work

--- app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
@@ -10,6 +10,10 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.dev.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
 
@@ -27,6 +31,16 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+        val workRequest = OneTimeWorkRequest
+            .Builder(PollWorker::class.java)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance(requireContext())
+            .enqueue(workRequest)
     }
 
     override fun onCreateView(
