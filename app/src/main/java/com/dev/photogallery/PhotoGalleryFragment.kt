f391a7ccce9816ef30027183b8586f8bf841828c
alice <aliceae.h@gmail.com>
2023-02-10 17:13:28 +0800

    [ch23]implicit intent: start the browser

--- app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.dev.photogallery
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -50,7 +51,12 @@ class PhotoGalleryFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 photoGalleryViewModel.uiState.collect { state ->
-                    binding.photoGrid.adapter = PhotoListAdapter(state.images)
+                    binding.photoGrid.adapter = PhotoListAdapter(
+                        state.images
+                    ) { photoPageUri ->
+                        val intent = Intent(Intent.ACTION_VIEW, photoPageUri)
+                        startActivity(intent)
+                    }
                     searchView?.setQuery(state.query, false)
                     updatePollingState(state.isPolling)
                 }
