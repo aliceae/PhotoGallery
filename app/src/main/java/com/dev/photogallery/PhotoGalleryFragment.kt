e3be4a0afcc1161f27c8b34d5874407d8d18ddce
alice <aliceae.h@gmail.com>
2023-02-14 16:26:04 +0800

    [ch23]chrome_custom_tabs

--- app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/dev/photogallery/PhotoGalleryFragment.kt
@@ -1,10 +1,13 @@
 package com.dev.photogallery
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.appcompat.widget.SearchView
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -55,11 +58,13 @@ class PhotoGalleryFragment : Fragment() {
                     binding.photoGrid.adapter = PhotoListAdapter(
                         state.images
                     ) { photoPageUri ->
-                        findNavController().navigate(
-                            PhotoGalleryFragmentDirections.showPhoto(
-                                photoPageUri
-                            )
-                        )
+                        CustomTabsIntent.Builder()
+                            .setToolbarColor(
+                                    ContextCompat.getColor(requireContext(),
+                                    R.color.colorPrimary))
+                            .setShowTitle(true)
+                            .build()
+                            .launchUrl(requireContext(), photoPageUri)
                     }
                     searchView?.setQuery(state.query, false)
                     updatePollingState(state.isPolling)
